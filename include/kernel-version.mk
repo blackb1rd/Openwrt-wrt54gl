71c8fd6e4ad38ca16912cd185157e9c659d81502
b1rd <b1rd@localhost.localdomain>
2013-08-25 08:49:41 +0700

    remove kernel 2.6

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -2,25 +2,11 @@
 
 ifeq ($(KERNEL),2.4)
   LINUX_VERSION?=2.4.35.4
-else
-  LINUX_VERSION?=2.6.26.8
 endif
 LINUX_RELEASE?=1
 
 ifeq ($(LINUX_VERSION),2.4.35.4)
   LINUX_KERNEL_MD5SUM:=0de40d83cb9a216583b8046825f4d991
-endif
-ifeq ($(LINUX_VERSION),2.6.23.17)
-  LINUX_KERNEL_MD5SUM:=a0300a393ac91ce9c64bf31522b45e2e
-endif
-ifeq ($(LINUX_VERSION),2.6.24.7)
-  LINUX_KERNEL_MD5SUM:=40a73780d51525d28d36dec852c680c4
-endif
-ifeq ($(LINUX_VERSION),2.6.25.20)
-  LINUX_KERNEL_MD5SUM:=0da698edccf03e2235abc2830a495114
-endif
-ifeq ($(LINUX_VERSION),2.6.26.8)
-  LINUX_KERNEL_MD5SUM:=05dd0d4f8f110b4219ae6ec7a36c046d
 endif
 
 # disable the md5sum check for unknown kernel versions
